7b1cc14a9c628d6a3926a2760850b164eb4938d8
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-11-13 23:14:27 +0530

    Addded Base file for Intro,Login

---
+++ app/src/main/java/com/codeinflow/gratify/Intro3.java
@@ -0,0 +1,26 @@
+package com.codeinflow.gratify;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Intro3 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_intro3, container, false);
+    }
+
+}
